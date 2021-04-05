6cf7b7e728771978ad200bdeda0d5720bc10cee9
a-kmck <annakmieciak12@gmail.com>
2021-04-04 22:03:09 -0400

    Added more greetings

--- src/bot.js
+++ src/bot.js
@@ -24,11 +24,16 @@ client.on('message', (message) => {
         
         
         if(CMD_NAME === 'kick'){
+            if (!message.member.hasPermission('KICK_MEMBERS'))
+                return message.reply('You do not have persmission to use that command');
             if (args.length === 0) return message.reply('Please provide an ID');
             const member = message.guild.members.cache.get(args[0]);
 
             if (member){
-                member.kick();
+                member
+                .kick()
+                .then((member) => message.channel.send(`${member} was kicked.`))
+                .catch((err) => message.channel.send('I do not have permissions'));
             }
             else{
                 message.channel.send('That member was not found');
